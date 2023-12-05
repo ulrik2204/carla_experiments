b0cd9e009ce58aafbef95f125beff560d7e7a84d
Ulrik Røsby <ulrik@rosby.no>
2023-12-05 12:55:13 +0100

    refactor: folder restructure

--- carla_experiments/models/simple.py
+++ carla_experiments/models/simple.py
@@ -2,14 +2,19 @@ import torch
 import torch.nn as nn
 from torchvision.models import ResNet50_Weights, resnet50
 
+trans = ResNet50_Weights.IMAGENET1K_V2.transforms
+
 
 class SimpleLineFollowing(nn.Module):
     def __init__(
-        self, output_dim: int, use_weights: bool = True, *args, **kwargs
+        self, output_dim: int, use_pretrained_weights: bool = True, *args, **kwargs
     ) -> None:
         super().__init__(*args, **kwargs)
-        # TODO: Test with and without pretrained weights
-        self.resnet = resnet50(weights=ResNet50_Weights.DEFAULT, **kwargs)
+        pretrained_weights = (
+            ResNet50_Weights.IMAGENET1K_V2 if use_pretrained_weights else None
+        )
+        # Torchvision's resnet50 transforms the input images to 3x224x224
+        self.resnet = resnet50(weights=pretrained_weights, **kwargs)
         self.resnet.fc = torch.nn.Identity()  # type: ignore
         self.regressor = nn.Sequential(
             nn.Linear(2048, 64),
---
+++ carla_experiments/train/train_simple.py
@@ -0,0 +1,6 @@
+def main():
+    print("Training")
+
+
+if __name__ == "__main__":
+    main()
