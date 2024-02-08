fix: make frame counts consistent, try fix rotation

carla_experiments/carla_utils/setup.py
@@ -1,6 +1,7 @@
 import sys
 import time
 from functools import wraps
+from glob import glob
 from pathlib import Path
 from queue import Empty, Queue
 from typing import Any, Callable, Dict, List, Mapping, Optional, Type, TypeVar, cast
@@ -213,7 +214,7 @@ def game_loop_segment(
             context.client.get_world().tick()
             frames += 1
         except KeyboardInterrupt:
-            _stop_loop(context, save_files_base_path, on_finished, cleanup_actors)
+            _stop_loop(context, save_files_base_path, None, cleanup_actors)
             sys.exit()
 
 
@@ -243,16 +244,25 @@ TSettings = TypeVar("TSettings")
 
 def save_items_to_file(base_path: Path, items: SaveItems):
     base_path.mkdir(parents=True, exist_ok=True)
-    for path, value in items.items():
-        path_to_save = base_path / path
-        if isinstance(value, dict):
-            save_items_to_file(path_to_save, value)
-        elif isinstance(value, np.ndarray):
-            np.save(path_to_save, value)
-        elif isinstance(value, carla.Image):
-            value.save_to_disk(path_to_save.as_posix())
-        else:
-            raise ValueError(f"Unknown save item type {type(value)}")
+    item_dicts = [(base_path, items)]
+    files: List[Path] = []
+    while True:
+        used_base_path, used_items = item_dicts.pop(0)
+        for path, value in used_items.items():
+            path_to_save = used_base_path / path
+            if isinstance(value, dict):
+                path_to_save.mkdir(parents=True, exist_ok=True)
+                item_dicts.append((path_to_save, value))
+            elif isinstance(value, np.ndarray):
+                np.save(path_to_save, value)
+                files.append(path_to_save)
+            elif isinstance(value, carla.Image):
+                value.save_to_disk(path_to_save.as_posix())
+                files.append(path_to_save)
+            else:
+                raise ValueError(f"Unknown save item type {type(value)}")
+        if len(item_dicts) == 0:
+            break
 
 
 def create_dataset(
@@ -341,24 +351,23 @@ def segment(
             cleanup_actors = (
                 optionals["cleanup_actors"] if "cleanup_actors" in optionals else False
             )
+            on_finish_save_files = (
+                optionals["on_finish_save_files"]
+                if "on_finish_save_files" in optionals
+                else None
+            )
 
-            def on_end(context: TContext, save_files_base_path: Path):
-                on_finish_save_files = (
-                    optionals["on_finish_save_files"]
-                    if "on_finish_save_files" in optionals
-                    else None
-                )
+            on_exit = (
+                optionals["on_segment_end"] if "on_segment_end" in optionals else None
+            )
+
+            def on_end(ctx: TContext, save_files_base_path: Path):
                 if on_finish_save_files is not None:
-                    save_items = on_finish_save_files(context)
+                    save_items = on_finish_save_files(ctx)
                     save_items_to_file(save_files_base_path, save_items)
                     # To make sure the files are written to disk before next step
-                on_exit = (
-                    optionals["on_segment_end"]
-                    if "on_segment_end" in optionals
-                    else None
-                )
                 if on_exit is not None:
-                    on_exit(context, save_files_base_path)
+                    on_exit(ctx, save_files_base_path)
 
             game_loop_segment(
                 context=context,

carla_experiments/datagen/utils.py
@@ -7,6 +7,7 @@ import cv2
 import ffmpeg
 import numpy as np
 import pymap3d as pm
+from scipy.spatial.transform import Rotation as R
 
 
 def frames_to_video(
@@ -66,16 +67,10 @@ def frames_to_video(
 def euler_to_quaternion(rotation: carla.Rotation):
     # I have confirmed that this works the same way OP-Deepdive does it
     # Convert degrees to radians
-    # pitch = z, yaw = y, roll = z rotation
-    # Converting from left-handed to right-handed coordinate system negates yaw and roll
-    # Swap pitch and roll to convert from Unreal Engine to ECEECEF
-    converted_pitch = -rotation.roll
-    converted_yaw = -rotation.yaw
-    converted_roll = rotation.pitch
 
-    pitch = np.radians(converted_pitch)
-    yaw = np.radians(converted_yaw)
-    roll = np.radians(converted_roll)
+    pitch = -np.radians(rotation.pitch)
+    yaw = -np.radians(rotation.yaw) + np.pi / 2
+    roll = np.radians(rotation.roll)
 
     # Pre-calculate sine and cosine for pitch, yaw, and roll
     cy = np.cos(yaw * 0.5)
@@ -97,16 +92,125 @@ def euler_to_quaternion(rotation: carla.Rotation):
     return np.array([w, x, y, z])
 
 
+def euler_to_quaternion2(map: carla.Map, rotation: carla.Rotation):
+    origin_geolocation = map.transform_to_geolocation(carla.Location(0, 0, 0))
+    lat0 = origin_geolocation.latitude
+    lon0 = origin_geolocation.longitude
+    # alt0 = origin_geolocation.altitude
+
+    # I know this rot matrix works in the original implementation
+    # Construct rotation matrix from ECEF to ENU frames
+    # https://gssc.esa.int/navipedia/index.php/Transformations_between_ECEF_and_ENU_coordinates
+    # r = R.from_matrix([
+    #     [-np.sin(lon0),  -np.cos(lon0)*np.sin(lat0),   np.cos(lon0)*np.cos(lat0)],
+    #     [np.cos(lon0),   -np.sin(lon0)*np.sin(lat0),   np.sin(lon0)*np.cos(lat0)],
+    #     [0,               np.cos(lat0),                np.sin(lat0)             ]]).inv()
+
+    # I think we should remove .inv() because I think original example was from ECEF to ENU
+    # TODO: Add conversion of alt0 as well (Ulrik)
+    r = R.from_matrix(
+        [
+            [-np.sin(lon0), -np.cos(lon0) * np.sin(lat0), np.cos(lon0) * np.cos(lat0)],
+            [np.cos(lon0), -np.sin(lon0) * np.sin(lat0), np.sin(lon0) * np.cos(lat0)],
+            [0, np.cos(lat0), np.sin(lat0)],
+        ]
+    )
+    # https://carla.readthedocs.io/en/latest/python_api/#carlarotation
+    # https://se.mathworks.com/help/uav/ug/coordinate-systems-for-unreal-engine-simulation-in-uav-toolbox.html
+    # In carla
+    # pitch is right-handed rotation around the y-axis
+    # yaw is left-handed rotation around the z-axis
+    # roll is right-handed rotation around the x-axis
+    # To adjust for the difference in coordinate systems, we need to negate pitch and yaw
+    x = np.radians(rotation.roll)
+    y = -np.radians(rotation.pitch)
+    z = -np.radians(rotation.yaw)
+
+    # Not tested
+    enu_rot = R.from_euler("xyz", (x, y, z), degrees=False)
+    ecef_rot = enu_rot.as_matrix() @ r.as_matrix()
+    # This returns the quaternion on the [x, y, z, w] format
+    ecef_orientation = R.from_matrix(ecef_rot).as_quat()
+    x = ecef_orientation[0]
+    y = ecef_orientation[1]
+    z = ecef_orientation[2]
+    w = ecef_orientation[3]
+
+    # print(f"{ecef_orientation = }")
+    return np.array([w, x, y, z])
+
+
+def carla_location_to_ecef2(map: carla.Map, location: carla.Location):
+    origin_geolocation = map.transform_to_geolocation(carla.Location(0, 0, 0))
+    lat0 = np.radians(origin_geolocation.latitude)
+    lon0 = np.radians(origin_geolocation.longitude)
+    alt0 = np.radians(origin_geolocation.altitude)
+    x0, y0, z0 = pm.geodetic2ecef(lat0, lon0, alt0, deg=False)
+    r = R.from_matrix(
+        [
+            [-np.sin(lon0), -np.cos(lon0) * np.sin(lat0), np.cos(lon0) * np.cos(lat0)],
+            [np.cos(lon0), -np.sin(lon0) * np.sin(lat0), np.sin(lon0) * np.cos(lat0)],
+            [0, np.cos(lat0), np.sin(lat0)],
+        ]
+    )
+    enu_pos = np.array([location.x, -location.y, location.z])
+    ecef_pos = enu_pos @ r.as_matrix() + (x0, y0, z0)
+    return ecef_pos
+
+
+def something():
+    # Does not do anything in the gist
+    # lat0, lon0, _ = pymap3d.ecef2geodetic(x0, y0, z0, deg=False)
+
+    # Origin of the ENU frame
+    lat0, lon0 = (63.421639, 10.428341)
+    x0, y0, z0 = pm.geodetic2ecef(lat0, lon0, 0)
+    print(x0, y0, z0)
+
+    # I know this rot matrix works in the original implementation
+    # Construct rotation matrix from ECEF to ENU frames
+    # https://gssc.esa.int/navipedia/index.php/Transformations_between_ECEF_and_ENU_coordinates
+    # r = R.from_matrix([
+    #     [-np.sin(lon0),  -np.cos(lon0)*np.sin(lat0),   np.cos(lon0)*np.cos(lat0)],
+    #     [np.cos(lon0),   -np.sin(lon0)*np.sin(lat0),   np.sin(lon0)*np.cos(lat0)],
+    #     [0,               np.cos(lat0),                np.sin(lat0)             ]]).inv()
+
+    # I think we should remove .inv() because I think original example was from ECEF to ENU
+    # TODO: Add conversion of alt0 as well (Ulrik)
+    r = R.from_matrix(
+        [
+            [-np.sin(lon0), -np.cos(lon0) * np.sin(lat0), np.cos(lon0) * np.cos(lat0)],
+            [np.cos(lon0), -np.sin(lon0) * np.sin(lat0), np.sin(lon0) * np.cos(lat0)],
+            [0, np.cos(lat0), np.sin(lat0)],
+        ]
+    )
+
+    # Not tested
+    enu_pos = np.array([0, 0, 0])
+    ecef_pos = enu_pos @ r.as_matrix() + (x0, y0, z0)
+
+    # Not tested
+    enu_rot = R.from_euler("xyz", (0, 0, 0), degrees=False)
+    ecef_rot = enu_rot.as_matrix() @ r.as_matrix()
+    ecef_orientation = R.from_matrix(ecef_rot).as_quat()
+
+    print(f"{ecef_pos = }")
+    print(f"{ecef_orientation = }")
+
+
 def carla_location_to_ecef(map: carla.Map, location: carla.Location):
     # I want to check if this finds the origin
     origin_geolocation = map.transform_to_geolocation(carla.Location(0, 0, 0))
     # geolocation = map.transform_to_geolocation(location)
     # https://docs.unrealengine.com/4.27/en-US/BuildingWorlds/Georeferencing/
+    e_enu = location.x
+    n_enu = -location.y
+    u_enu = location.z
 
     x, y, z = pm.enu2ecef(
-        location.x,
-        -location.y,  # Negate y to convert from right-handed to left-handed coordinate system
-        location.z,
+        e_enu,
+        n_enu,  # Negate y to convert from right-handed to left-handed coordinate system
+        u_enu,
         origin_geolocation.latitude,
         origin_geolocation.longitude,
         origin_geolocation.altitude,