8a75ccb30c383b4f39d08720c354a2010c1c8995
Ulrik Røsby <ulrik@rosby.no>
2024-02-19 13:20:46 +0100

    fix: restart carla also in generate script

--- generate.sh
+++ generate.sh
@@ -7,16 +7,15 @@ else
     root_folder=$1
 fi
 comma_folder="$root_folder/carla2k19"
-echo "Starting at in $root_folder"
-nohup sh ../Carla2/CARLA_0.9.15/CarlaUE4.sh &
-sleep 10
 
-max_attempts=500
+max_attempts=10
 attempt_num=1
 
 while true; do
     echo "Attempt $attempt_num"
-    # Run your command here
+    echo "Starting at in $root_folder"
+    nohup sh ../Carla2/CARLA_0.9.15/CarlaUE4.sh &
+    sleep 10
     poetry run python -m carla_experiments.datagen.generate_comma2k19_data --root-folder=$comma_folder --progress-file="./progress-02-15_19-32.txt"  # TODO: Add args
     
     # Check the exit status of the command
@@ -24,18 +23,19 @@ while true; do
         echo "Command succeeded."
         break
     else
-        echo "Command failed. Retrying..."
         ((attempt_num++))
         # Break the loop if the number of attempts exceeds the maximum
         if [ $attempt_num -gt $max_attempts ]; then
             echo "Maximum attempts reached. Exiting."
             break
+        else
+            echo "Command failed. Retrying..."
+            sleep 2
+            npx kill-port 2000
+            sleep 2
         fi
     fi
-    
-    # Optional: sleep before retrying
-    sleep 1
 done
 poetry run python -m carla_experiments.datagen.extract_comma2k19 --root-folder=$root_folder --comma2k19-folder=$comma_folder  # TODO: Add args
-sleep 5
+sleep 2
 npx kill-port 2000
