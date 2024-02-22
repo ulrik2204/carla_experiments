406fcc4ed5a0ac057bf3400de5a30af42fff31a1
Ulrik Røsby <ulrik@rosby.no>
2024-02-22 18:35:29 +0100

    feat: exit generate script on ctrl+c

--- generate.sh
+++ generate.sh
@@ -1,7 +1,18 @@
 #! /bin/bash
-# this_time = $(date)
+
+# Define a function to execute when SIGINT is caught
+cleanup_and_exit() {
+    echo "Interrupt received. Exiting..."
+    # Perform any necessary cleanup here
+    npx kill-port 2000
+    # Exit the script
+    exit 1
+}
+
+# Trap SIGINT and call the cleanup_and_exit function when caught
+trap cleanup_and_exit SIGINT
+
 if [ $# -eq 0 ]; then
-    
     root_folder="output/$(date +"%Y-%m-%d-%H-%M-%S")"
 else
     root_folder=$1
@@ -13,7 +24,7 @@ attempt_num=1
 
 while true; do
     echo "Attempt $attempt_num"
-    echo "Starting at in $root_folder"
+    echo "Starting in $root_folder"
     nohup sh ../Carla2/CARLA_0.9.15/CarlaUE4.sh &
     sleep 10
     poetry run python -m carla_experiments.datagen.generate_comma2k19_data --root-folder=$comma_folder --progress-file="./progress-02-15_19-32.txt"  # TODO: Add args
@@ -24,7 +35,6 @@ while true; do
         break
     else
         ((attempt_num++))
-        # Break the loop if the number of attempts exceeds the maximum
         if [ $attempt_num -gt $max_attempts ]; then
             echo "Maximum attempts reached. Exiting."
             break
@@ -36,6 +46,7 @@ while true; do
         fi
     fi
 done
+
 poetry run python -m carla_experiments.datagen.extract_comma2k19 --root-folder=$root_folder --comma2k19-folder=$comma_folder  # TODO: Add args
 sleep 2
 npx kill-port 2000
