5380b19a214a2f1a4bc28b21ac71a43a3d58805e
Ulrik Røsby <ulrik@rosby.no>
2024-04-16 12:31:17 +0200

    fix: update download_github_file with steps to downlaod from git-lfs as well

--- carla_experiments/custom_logreader/download_latest_capnp.py
+++ carla_experiments/custom_logreader/download_latest_capnp.py
@@ -1,4 +1,4 @@
-import requests
+from carla_experiments.common.openpilot_repo import download_github_file
 
 # GitHub repository details
 USER = "commaai"
@@ -11,31 +11,16 @@ REPO_URL = f"https://api.github.com/repos/{USER}/{REPO}"
 LATEST_COMMIT_URL = f"{REPO_URL}/commits/{BRANCH}"
 
 
-def download_file(url, file_name):
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open(file_name, "wb") as f:
-            f.write(response.content)
-        print(f"Downloaded {file_name}")
-    else:
-        print(f"Failed to download {file_name}: HTTP {response.status_code}")
-
-
 def main():
-    # Fetch the latest commit SHA
-    response = requests.get(LATEST_COMMIT_URL)
-    if response.status_code == 200:
-        commit_sha = response.json()["sha"]
-        print(f"Latest commit SHA: {commit_sha}")
-
-        # Download files using the latest commit SHA
-        for file in FILES:
-            file_url = (
-                f"https://raw.githubusercontent.com/{USER}/{REPO}/{commit_sha}/{file}"
-            )
-            download_file(file_url, "new_" + file)
-    else:
-        print(f"Failed to fetch latest commit: HTTP {response.status_code}")
+    for file in FILES:
+        save_path = f"carla_experiments/custom_logreader/new_{file}"
+        download_github_file(
+            repo_owner=USER,
+            repo_name=REPO,
+            file_path_in_repo=file,
+            save_path=save_path,
+            main_branch_name=BRANCH,
+        )
 
 
 if __name__ == "__main__":
